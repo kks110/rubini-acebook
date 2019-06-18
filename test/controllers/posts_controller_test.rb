628e72c485c013990849b0714eb57c5733a00d6f
Ollie Porter <ol.porter@me.com>
2019-06-18 15:09:55 +0100

    Create feature test for new post

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get new post" do
+    get new_post_url
+    assert_response :success
+  end
+  test "should get edit post" do
+    get edit_post_url
+    assert_response :success
+  end
+  test "should get posts" do
+    get posts_url
+    assert_response :success
+  end
+  test "should show individual post" do
+    get post_url
+    assert_response :success
+  end
 end
