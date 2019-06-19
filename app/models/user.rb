5d46ec69ea5d389fec5adc9eb08c107ebd634fbb
JAYDA KADIR <jydkdr@gmail.com>
2019-06-19 16:58:18 +0100

    Add system and model tests for adding users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+
+ before_save { self.email = email.downcase }
+ before_save { self.username = username.downcase }
+
+ validates :username, presence: true,
+            length: { minimum: 2, maximum: 15},
+            uniqueness: true
+
+
+ VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+ validates :email, presence: true,
+            uniqueness: true,
+            format: { with: VALID_EMAIL_REGEX }
+
+validates :password_digest, presence: true,
+           length: { minimum: 6, maximum: 25}
+
+ has_secure_password
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get sign up page" do
+    get signup_url
+    assert_response :success
+  end
+
+  test "should get log in page" do
+    get login_url
+    assert_response :success
+  end
+
 end
