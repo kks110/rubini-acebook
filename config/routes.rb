56ec31375f210678be0880e7c2672b2326cda433
Kelvin Samuel <kelvin@kks110.com>
2019-06-24 16:07:48 +0100

    Cleaned up routes and fixed typo

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   resources :users
   get 'login' => 'sessions#new'
-  resources :sessions
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
